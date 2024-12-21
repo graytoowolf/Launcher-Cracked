0b8dbd3f97e02bd08303b6f7b1a71133841a68e4
Petr Mrázek <peterix@gmail.com>
2024-12-21 15:08:35 +0100

    GH-4155 Move 'change group' to a section of the sidebar where it makes more sense

--- launcher/ui/MainWindow.cpp
+++ launcher/ui/MainWindow.cpp
@@ -508,6 +508,13 @@ public:
         renameButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
         instanceToolBar->addWidget(renameButton);
 
+        actionChangeInstGroup = TranslatedAction(MainWindow);
+        actionChangeInstGroup->setObjectName(QStringLiteral("actionChangeInstGroup"));
+        actionChangeInstGroup.setTextId(QT_TRANSLATE_NOOP("MainWindow", "Change Group"));
+        actionChangeInstGroup.setTooltipId(QT_TRANSLATE_NOOP("MainWindow", "Change the selected instance's group."));
+        all_actions.append(&actionChangeInstGroup);
+        instanceToolBar->addAction(actionChangeInstGroup);
+
         instanceToolBar->addSeparator();
 
         actionLaunchInstance = TranslatedAction(MainWindow);
@@ -558,13 +565,6 @@ public:
         actionScreenshots.setTooltipId(QT_TRANSLATE_NOOP("MainWindow", "View and upload screenshots for this instance."));
         all_actions.append(&actionScreenshots);
         instanceToolBar->addAction(actionScreenshots);
-
-        actionChangeInstGroup = TranslatedAction(MainWindow);
-        actionChangeInstGroup->setObjectName(QStringLiteral("actionChangeInstGroup"));
-        actionChangeInstGroup.setTextId(QT_TRANSLATE_NOOP("MainWindow", "Change Group"));
-        actionChangeInstGroup.setTooltipId(QT_TRANSLATE_NOOP("MainWindow", "Change the selected instance's group."));
-        all_actions.append(&actionChangeInstGroup);
-        instanceToolBar->addAction(actionChangeInstGroup);
 
         instanceToolBar->addSeparator();
 
